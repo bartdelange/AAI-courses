cea38af89a94b3c6d879f072d1e645b319f69aef
Bart de Lange <bart.de.lange32@gmail.com>
2019-03-02 00:37:36 +0100

    Optimized Manhattan so that straight lines are actually straight
    If you placed start and target in a direct vertical or horizontal line it went diagonal straight diagonal

--- src/AICore/Graph/Manhattan.cs
+++ src/AICore/Graph/Manhattan.cs
@@ -7,7 +7,7 @@ namespace AICore.Graph
     {
         public double Calculate(Vector2 v1, Vector2 v2)
         {
-            return Math.Abs(v1.X - v2.X + (v1.Y - v2.Y));
+            return Math.Abs(v1.X - v2.X) + Math.Abs(v1.Y - v2.Y);
         }
     }
 }
