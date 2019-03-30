f5d4c16a4d8cccb12e909b226073fea127c7acd1
Leroy Korterink <leroy@mediamonks.com>
2019-03-30 23:47:12 +0100

    Fix Activate behaviour

--- src/AICore/Behaviour/Goals/StrikerGoals/StrikerThink.cs
+++ src/AICore/Behaviour/Goals/StrikerGoals/StrikerThink.cs
@@ -1,48 +1,16 @@
-using System;
 using AICore.Entity.Contracts;
-using AICore.Exceptions;
-using AICore.FuzzyLogic;
 using AICore.Worlds;
 
 namespace AICore.Behaviour.Goals.StrikerGoals
 {
-    public class StrikerThink : BaseGoal
+    public class StrikerThink : Think
     {
-        private FuzzyModule _fmBall = new FuzzyModule();
-        private FuzzyModule _fmPosition = new FuzzyModule();
-        private TiredModule _tiredModule = new TiredModule();
-
         public StrikerThink(IPlayer player, SoccerField soccerField): base(player, soccerField)
         {
+            ActiveGoal = new RestGoal(Player, SoccerField);
+                
             Add(GoalNames.GoToBall, new GoToBall(player, soccerField));
             Add(GoalNames.DribbleToGoal, new DribbleToGoal(player, soccerField));
-        }
-        
-        public override void Update(IPlayer player)
-        {
-            IGoal desirableGoal = new RestGoal(Player, SoccerField);
-            var currentDesirability = desirableGoal.CheckDesirability();
-            
-            foreach (var goal in _goals)
-            {
-                if (goal.Value.CheckDesirability() > currentDesirability)
-                {
-                    desirableGoal = goal.Value;
-                }
-            }
-
-            desirableGoal.Activate();
-            desirableGoal.Update(player);
-        }
-
-        public override void Activate()
-        {
-            throw new NotImplementedByException(); // Not needed
-        }
-
-        public override double CheckDesirability()
-        {
-            throw new NotImplementedByException(); // Not needed
         }
     }
 }
---
+++ src/AICore/Behaviour/Goals/StrikerGoals/Think.cs
@@ -0,0 +1,64 @@
+using System;
+using AICore.Entity.Contracts;
+using AICore.Exceptions;
+using AICore.Worlds;
+
+namespace AICore.Behaviour.Goals.StrikerGoals
+{
+    public abstract class Think : BaseGoal
+    {
+        protected IGoal ActiveGoal
+        {
+            private get
+            {
+                if (_activeGoal == null)
+                {
+                    throw new ArgumentNullException("Default goal not set.");
+                }
+                
+                return _activeGoal;
+            }
+            
+            set => _activeGoal = value;
+        }
+        
+        private IGoal _activeGoal;
+        
+        public Think(IPlayer player, SoccerField soccerField) : base(player, soccerField)
+        {
+        }
+
+        public override void Update(IPlayer player)
+        {
+            var desirableGoal = ActiveGoal;
+            var currentDesirability = desirableGoal.CheckDesirability();
+            
+            foreach (var goal in _goals)
+            {
+                if (goal.Value.CheckDesirability() > currentDesirability)
+                {
+                    desirableGoal = goal.Value;
+                }
+            }
+
+            if (desirableGoal != ActiveGoal)
+            {
+                desirableGoal.Activate();                
+                ActiveGoal = desirableGoal;
+            }
+
+            desirableGoal.Update(player);
+        }
+
+        public override void Activate()
+        {
+            
+            throw new NotImplementedByException(); // Not needed
+        }
+
+        public override double CheckDesirability()
+        {
+            throw new NotImplementedByException(); // Not needed
+        }
+    }
+}
